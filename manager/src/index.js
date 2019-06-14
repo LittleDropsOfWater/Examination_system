a7cde579470e8aead917b5710b0e997a851d1899
LittleDropsOfWater <651027540@qq.com>
2019-06-14 19:03:48 +0800

    设置redux-logger

--- manager/src/index.js
+++ manager/src/index.js
@@ -1,12 +1,19 @@
 import dva from 'dva';
 import './index.css';
-import loading from "dva-loading"
+import loading from "dva-loading";
+import createLogger from 'redux-logger';
+import {message} from 'antd';
 // 1. Initialize
 const app = dva();
 
 // 2. Plugins
 app.use(loading());
-
+app.use({
+	onAction:createLogger(),
+	onError(e) {
+    message.error(e.message, /* duration */3);
+  }
+})
 // 3. Model
 // app.model(require('./models/example').default);
 app.model(require('./models/user').default);
