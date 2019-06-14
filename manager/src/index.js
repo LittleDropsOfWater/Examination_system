20c599f4737226b4523f04e9d6e7387127b5637e
LittleDropsOfWater <651027540@qq.com>
2019-06-14 19:09:15 +0800

    'redux-logger'

--- manager/src/index.js
+++ manager/src/index.js
@@ -1,19 +1,20 @@
 import dva from 'dva';
 import './index.css';
 import loading from "dva-loading";
-import createLogger from 'redux-logger';
+import {createLogger} from 'redux-logger';
 import {message} from 'antd';
 // 1. Initialize
 const app = dva();
 
 // 2. Plugins
-app.use(loading());
 app.use({
 	onAction:createLogger(),
 	onError(e) {
     message.error(e.message, /* duration */3);
   }
 })
+app.use(loading());
+
 // 3. Model
 // app.model(require('./models/example').default);
 app.model(require('./models/user').default);
